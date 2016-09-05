4f3b2e9168e3e942c1d03bd2c9b1672b4c4a963d
MagikCow <maxwilkinsonasd@gmail.com>
2016-09-05 16:44:39 +0100

    major speed update
    doubled performance for 10 digit prime number.

--- code/is_prime.py
+++ code/is_prime.py
@@ -5,15 +5,23 @@
 # is_prime(n) where n is the number to determine primeness
 
 import is_whole
+import math
 
 def is_prime(n):
-    if n <= 1 or is_whole(n) == False:
+    
+    sqrt_n = int(math.sqrt(n) + 2) # number to test up to
+    
+    if n <= 1 or is_whole.is_whole(n) == False: # If n is less than 1 or not whole
         return False
 
+    if n == 2:
+        return True
+
     else:
-        for i in range(2, n):
-            if n % i == 0:
+        for i in range(2, sqrt_n): # generate list of all numbers up to sqrt(n)
+                        
+            if n % i == 0: # if n goes into i n is not prime
                 return False
 
-            if i == n - 1:
+            if i == (sqrt_n - 1): # if this is reached no number between n is prime
                 return True
