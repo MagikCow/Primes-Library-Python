25042da1e72ac79375952381b2c9b407a3138110
MagikCow <maxwilkinsonasd@gmail.com>
2016-09-12 18:00:51 +0100

    faster method
    a number is only prime if it is of the form 6k ± 1. So we test if n is of the form 6k±1 and then test if it has factors (not prime) or not (prime).

--- code/is_prime.py
+++ code/is_prime.py
@@ -7,21 +7,25 @@
 import is_whole
 import math
 
+
 def is_prime(n):
-    
-    sqrt_n = int(math.sqrt(n) + 2) # number to test up to
-    
-    if n <= 1 or is_whole.is_whole(n) == False: # If n is less than 1 or not whole
+    sqrt_n = int(math.sqrt(n) + 2)
+
+    if n <= 1 or is_whole.is_whole(n) == False:  # If n is less than 1 or not whole
         return False
 
-    if n == 2:
+    if n == 2 or n == 3:
         return True
 
     else:
-        for i in range(2, sqrt_n): # generate list of all numbers up to sqrt(n)
-                        
-            if n % i == 0: # if n goes into i n is not prime
-                return False
+        if n % 6 == 1 or n % 6 == 5: # Al primes are of form (6k +- 1)
 
-            if i == (sqrt_n - 1): # if this is reached no number between n is prime
-                return True
+            for i in range(2, sqrt_n):  # generate list of all numbers up to sqrt(n)
+
+                if n % i == 0:  # if n goes into i n is not prime
+                    return False
+
+                if i == (sqrt_n - 1):  # if this is reached no number between n is prime
+                    return True
+        else:
+            return False
