250e589df145bac9cd362615a9cfd24ec0a0c0cc
MagikCow <maxwilkinsonasd@gmail.com>
2016-09-13 17:21:59 +0100

    bug fix

--- code/next_prime.py
+++ code/next_prime.py
@@ -5,7 +5,7 @@ import is_prime
 
 def next_prime(n):
     #Generate every number bigger than n
-    for i in range(n + 1, i + 1):
+    for i in range(n + 1, n**100):
 
         if is_prime.is_prime(i) == False:
             continue
